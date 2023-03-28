a56f31c3795ce752681ccd0bbf5145943eaf809c
yk <yosizuka1@gmail.com>
2023-03-28 17:06:06 +0900

    cal

--- tests/Unit/CalculatePointServiceTest.php
+++ tests/Unit/CalculatePointServiceTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Exceptions\PreconditionException;
 use PHPUnit\Framework\TestCase;
 use App\Services\CalculatePointService;
 
@@ -29,5 +30,58 @@ class CalculatePointServiceTest extends TestCase
     {
         $result = CalculatePointService::calcPoint(1000);
         $this->assertSame(10, $result);
+    }
+    /**
+     * @test
+     * @dataProvider dataProvider_for_calcPoint
+     */
+    public function calcPoint(int $expected, int $amount)
+    {
+        $reuslt = CalculatePointService::calcPoint($amount);
+        $this->assertSame($expected, $reuslt);
+    }
+    /**
+     * @test
+     */
+    public function exception_try_catch()
+    {
+        try {
+            throw new \InvalidArgumentException('message', 200);
+            $this->fail();
+        } catch (\Throwable $th) {
+            $this->assertInstanceOf(\InvalidArgumentException::class, $th);
+            $this->assertSame(200, $th->getCode());
+            $this->assertSame('message', $th->getMessage());
+            //throw $th;
+        }
+    }
+    /**
+     * @test
+     */
+    public function exception_expecException_method()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionCode(200);
+        $this->expectExceptionMessage('message');
+        throw new \InvalidArgumentException('message', 200);
+    }
+    /**
+     * @test
+     */
+    public function calcPoint_購入金額が負の数なら例外をスロー()
+    {
+        $this->expectException(PreconditionException::class);
+        $this->expectExceptionMessage('購入金額が負の数');
+        CalculatePointService::calcPoint(-1);
+    }
+    public function dataProvider_for_calcPoint(): array
+    {
+        return [
+            '購入金額が0なら0ポイント' => [0, 0],
+            '購入金額が999なら0ポイント' => [0, 999],
+            '購入金額が1000なら10ポイント' => [10, 1000],
+            '購入金額が9999なら99ポイント' => [99, 9999],
+            '購入金額が10000なら200ポイント' => [200, 10000],
+        ];
     }
 }
